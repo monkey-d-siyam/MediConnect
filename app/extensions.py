cbe1d96fe17933555b0d9aeb22d69d00f8db0561
SheikhTrump <134137837+SheikhTrump@users.noreply.github.com>
2025-04-28 23:14:11 +0600

    Delete app directory

--- app/extensions.py
+++
@@ -1,5 +0,0 @@
-from flask_mysqldb import MySQL
-from flask_mail import Mail
-
-mysql = MySQL()
-mail = Mail()
--- app/forms/appointment_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class AppointmentForm(FlaskForm):
-    doctor_id = SelectField('Select Doctor', coerce=int, validators=[DataRequired()])
-    appointment_date = StringField('Appointment Date (YYYY-MM-DD)', validators=[DataRequired()])
-    submit = SubmitField('Book Appointment')
