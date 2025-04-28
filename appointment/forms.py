1d941f09253d7a51d005e1ba56ac49a49f8c5391
SheikhTrump <134137837+SheikhTrump@users.noreply.github.com>
2025-04-28 23:28:33 +0600

    Delete appointment directory

--- appointment/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class SearchDoctorForm(FlaskForm):
-    search = StringField('Search by Name or Specialization', validators=[DataRequired()])
-    submit = SubmitField('Search')
